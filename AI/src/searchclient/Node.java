748eb1749c6f9ebe9719347af4d0fb3d1970bf23
Toonwire <Toonwire@hotmail.com>
2018-02-07 01:56:05 +0100

    rng seed change

--- AI/src/searchclient/Node.java
+++ AI/src/searchclient/Node.java
@@ -9,7 +9,7 @@ import java.util.Random;
 import searchclient.Command.Type;
 
 public class Node {
-	private static final Random RND = new Random(2);
+	private static final Random RND = new Random(1);
 
 	public int agentRow;
 	public int agentCol;
